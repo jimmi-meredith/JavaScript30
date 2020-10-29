d40080924e806e0592f244f42a56846ca36d29f9
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-10-30 11:49:22 +1300

    create progress variable

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,3 +1,4 @@
 const player = document.querySelector('.player')
 const video = player.querySelector('.viewer')
 const progress = player.querySelector('.progess')
+const progressBar = player.querySelector('.progress__filled')
