f2c1cb4edd8aff51b9e62bdc02abe77e1d41cd4b
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-10-30 12:34:38 +1300

    Write skip function

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -1,6 +1,6 @@
 const player = document.querySelector('.player')
 const video = player.querySelector('.viewer')
-const progress = player.querySelector('.progess')
+const progress = player.querySelector('.progress')
 const progressBar = player.querySelector('.progress__filled')
 const toggle = player.querySelector('.toggle')
 const skipButtons = player.querySelectorAll('[data-skip]')
@@ -24,9 +24,14 @@ const handleRangeUpdate = event => {
   video[event.target.name] = event.target.value
 }
 
-const handleProgress = event => {
+const handleProgress = () => {
   const percent = (video.currentTime / video.duration) * 100
   progressBar.style.flexBasis = `${percent}%`
+}
+
+const scrub = event => {
+  const scrubTime = (event.offsetX / progress.offsetWidth) * video.duration
+  video.currentTime = scrubTime
 }
 
 video.addEventListener('click', togglePlay)
@@ -34,6 +39,7 @@ video.addEventListener('play', updateButton)
 video.addEventListener('pause', updateButton)
 video.addEventListener('timeupdate', handleProgress)
 toggle.addEventListener('click', togglePlay)
+progress.addEventListener('click', scrub)
 
 skipButtons.forEach(button => button.addEventListener('click', skip))
 ranges.forEach(range => range.addEventListener('change', handleRangeUpdate))
