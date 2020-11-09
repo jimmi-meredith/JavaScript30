fa373add43cf87598d4ac7e339a8006ffff0070f
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-10 09:47:27 +1300

    create download link when taking a photo

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -29,7 +29,11 @@ const takePhoto = () => {
   snap.currentTime = 0
   snap.play()
   const data = canvas.toDataURL('image/jpeg')
-  console.log(data)
+  const link = document.createElement('a')
+  link.href = data
+  link.setAttribute('download', 'handsome')
+  link.textContent = 'Download Image'
+  strip.insertBefore(link, strip.firstChild)
 }
 
 getVideo()
