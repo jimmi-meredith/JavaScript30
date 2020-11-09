85c8ea87e2ff7c79245389502ac92cf33b4db0d5
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-10 10:24:46 +1300

    write greenScreen function

--- 19 - Webcam Fun/scripts.js
+++ 19 - Webcam Fun/scripts.js
@@ -24,8 +24,8 @@ const paintToCanvas = () => {
     ctx.drawImage(video, 0, 0, width, height)
     let pixels = ctx.getImageData(0, 0, width, height)
     // pixels = redEffect(pixels)
-    pixles = rgbSplit(pixels)
-    ctx.globalAlpha = 0.1
+    // pixles = rgbSplit(pixels)
+    // ctx.globalAlpha = 0.1
     ctx.putImageData(pixels, 0, 0)
   }, 16)
 }
@@ -57,6 +57,27 @@ const rgbSplit = pixels => {
     pixels.data[i - 150] = pixels.data[i + 2]
   }
   return pixels
+}
+
+const greenScreen = pixels => {
+  const levels = {}
+  document.querySelectorAll('.rgb input').forEach(input => {
+    levels[input.name] = input.value
+  })
+  for (i = 0; i < pixels.data.length; i = i + 4) {
+    red = pixels.data[i + 0]
+    green = pixels.data[i + 1]
+    blue = pixels.data[i + 2]
+    alpha = pixels.data[i + 3]
+    if (red >= levels.rmin &&
+      green >= levels.gmin &&
+      blue >= levels.bmin &&
+      red <= levels.rmax &&
+      green <= levels.gmax &&
+      blue <= levels.bmax) {
+      pixels.data[i + 3] = 0
+    }
+  }
 }
 
 getVideo()
