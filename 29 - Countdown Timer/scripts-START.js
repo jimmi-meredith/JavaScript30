3d74b4334eca84885fc1e66d61fa10003e8065ba
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-26 09:16:46 +1300

    add event listener to each button

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,7 @@
 let countdown
 const timerDisplay = document.querySelector('.display__time-left')
 const endTime = document.querySelector('.display__end-time')
+const buttons = document.querySelectorAll('[data-time]')
 
 const timer = seconds => {
   const now = Date.now()
@@ -30,3 +31,9 @@ const displayEndTime = timestamp => {
   const minutes = end.getMinutes()
   endTime.textContent = `Be back at ${hour < 10 ? '0' : ''}${hour}:${minutes < 10 ? '0' : ''}${minutes}`
 }
+
+const startTimer = () => {
+
+}
+
+buttons.forEach(button => button.addEventListener('click', startTimer))
