001e7e441ce0b59d79f5e38001725d78f3f38ee1
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-26 08:52:12 +1300

    create countdown variable

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,4 +1,10 @@
+let countdown
+
 const timer = seconds => {
   const now = Date.now()
   const then = now + seconds * 1000
+  setInterval(() => {
+    const secondsLeft = (then - Date.now()) / 1000
+    if (secondsLeft <= 0) return
+  }, 1000)
 }
