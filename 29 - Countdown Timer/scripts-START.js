ab209d242ff2440616ca33686e0520d671ced355
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-26 08:54:11 +1300

    fix error stopping at one

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -5,7 +5,7 @@ const timer = seconds => {
   const then = now + seconds * 1000
   countdown = setInterval(() => {
     const secondsLeft = (then - Date.now()) / 1000
-    if (secondsLeft <= 0) {
+    if (secondsLeft < 0) {
       clearInterval(countdown)
     }
   }, 1000)
