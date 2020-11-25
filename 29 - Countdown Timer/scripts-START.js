8209ccafc4a7dd90cdb5149513a6787fa6666591
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-26 08:58:06 +1300

    start countdown immediatly

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -3,10 +3,16 @@ let countdown
 const timer = seconds => {
   const now = Date.now()
   const then = now + seconds * 1000
+  displayTimeLeft(seconds)
   countdown = setInterval(() => {
     const secondsLeft = (then - Date.now()) / 1000
     if (secondsLeft < 0) {
       clearInterval(countdown)
     }
+    displayTimeLeft(seconds)
   }, 1000)
+}
+
+const displayTimeLeft = seconds => {
+  console.log(seconds)
 }
