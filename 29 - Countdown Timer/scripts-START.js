020046d6995362a0e328f1bc3e548de33f7ac82e
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-26 09:13:23 +1300

    display the end time of when countdown ends

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -1,10 +1,12 @@
 let countdown
 const timerDisplay = document.querySelector('.display__time-left')
+const endTime = document.querySelector('.display__end-time')
 
 const timer = seconds => {
   const now = Date.now()
   const then = now + seconds * 1000
   displayTimeLeft(seconds)
+  displayEndTime(then)
   countdown = setInterval(() => {
     const secondsLeft = Math.round((then - Date.now()) / 1000)
     if (secondsLeft < 0) {
@@ -20,4 +22,11 @@ const displayTimeLeft = seconds => {
   const remainderSeconds = seconds % 60
   const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
   timerDisplay.textContent = display
+}
+
+const displayEndTime = timestamp => {
+  const end = new Date(timestamp)
+  const hour = end.getHours()
+  const minutes = end.getMinutes()
+  endTime.textContent = `Be back at ${hour}:${minutes}`
 }
