9385652ee310e73bf9c2c367d7cc88280d6d8238
Jimmi Meredith <jimmi.meredith@gmail.com>
2020-11-26 09:07:18 +1300

    countdown working

--- 29 - Countdown Timer/scripts-START.js
+++ 29 - Countdown Timer/scripts-START.js
@@ -6,16 +6,18 @@ const timer = seconds => {
   const then = now + seconds * 1000
   displayTimeLeft(seconds)
   countdown = setInterval(() => {
-    const secondsLeft = (then - Date.now()) / 1000
+    const secondsLeft = Math.round((then - Date.now()) / 1000)
     if (secondsLeft < 0) {
       clearInterval(countdown)
+      return
     }
-    displayTimeLeft(seconds)
+    displayTimeLeft(secondsLeft)
   }, 1000)
 }
 
 const displayTimeLeft = seconds => {
   const minutes = Math.floor(seconds / 60)
   const remainderSeconds = seconds % 60
-  console.log(seconds)
+  const display = `${minutes < 10 ? '0' : ''}${minutes}:${remainderSeconds < 10 ? '0' : ''}${remainderSeconds}`
+  timerDisplay.textContent = display
 }
